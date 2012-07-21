9349fba0fe6e571060fb00de927ac91ae065cba4
Tomás Lázaro <tomas@belfrygames.com>
2012-07-20 21:18:40 -0300

    Corrected Annotations in TypeInsight

--- src/main/scala/miscellaneous/serialization/TypeInsight.scala
+++ src/main/scala/miscellaneous/serialization/TypeInsight.scala
@@ -2,7 +2,7 @@ package miscellaneous.serialization
 
 import java.lang.reflect._
 import scala.collection.mutable.{ Queue, Stack, HashMap }
-import scala.annotation.Annotation
+import java.lang.annotation.Annotation
 
 case class TypeInsight(nodeName: String, typeInfo: ClassManifest[_], annotations: Set[Annotation] = Set.empty,
   adapters: Queue[Option[Adapter[_, _]]] = Queue.empty, fieldProxy: Option[FieldProxy] = None,
