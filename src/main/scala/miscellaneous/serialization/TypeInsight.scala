07424c52d0dd4324d1073930b4911e991eee343c
rcano <rcano@netlabs.com.uy>
2012-07-20 21:50:39 -0300

    Fixed wrong Annotation usage

--- src/main/scala/miscellaneous/serialization/TypeInsight.scala
+++ src/main/scala/miscellaneous/serialization/TypeInsight.scala
@@ -1,8 +1,8 @@
 package miscellaneous.serialization
 
 import java.lang.reflect._
+import java.lang.annotation.Annotation
 import scala.collection.mutable.{ Queue, Stack, HashMap }
-import scala.annotation.Annotation
 
 case class TypeInsight(nodeName: String, typeInfo: ClassManifest[_], annotations: Set[Annotation] = Set.empty,
   adapters: Queue[Option[Adapter[_, _]]] = Queue.empty, fieldProxy: Option[FieldProxy] = None,
