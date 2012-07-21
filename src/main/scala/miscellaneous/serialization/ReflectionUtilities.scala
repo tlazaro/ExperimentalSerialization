076edbfcf643264787d042c5674de1e16eb5d22f
rcano <rcano@netlabs.com.uy>
2012-07-20 21:51:12 -0300

    Changed Manifests for ClassManifests

--- src/main/scala/miscellaneous/serialization/ReflectionUtilities.scala
+++ src/main/scala/miscellaneous/serialization/ReflectionUtilities.scala
@@ -3,15 +3,15 @@ package miscellaneous.serialization
 import java.lang.reflect.{ Type, ParameterizedType }
 
 private[serialization] object ReflectionUtilities {
-  def calculateManifest(typpe: Type): Manifest[_] = {
+  def calculateManifest(typpe: Type): ClassManifest[_] = {
     println("type: " + typpe)
     typpe match {
-      case pt: ParameterizedType => new Manifest[AnyRef] {
+      case pt: ParameterizedType => new ClassManifest[AnyRef] {
         def erasure = pt.getRawType().asInstanceOf[Class[_]]
         override def typeArguments = pt.getActualTypeArguments().map(calculateManifest).toList
         override def toString = (if (erasure.isArray) "Array" else erasure.getName) + argString
       }
-      case clazz: Class[_] => Manifest.classType(clazz)
+      case clazz: Class[_] => ClassManifest.classType(clazz)
       case _ => throw new IllegalArgumentException
     }
   }
