727e75849439fec3c21a0c729c2aee9cad349c8e
Tomás Lázaro <tomas@belfrygames.com>
2012-08-01 11:47:19 -0300

    Added explicit usage of BuiltinTypeHandlers and removed the service registration from META-INF.

--- src/main/scala/miscellaneous/serialization/HandledTypesRegistry.scala
+++ src/main/scala/miscellaneous/serialization/HandledTypesRegistry.scala
@@ -9,35 +9,36 @@ object HandledTypesRegistry {
 
   private[this] var registeredTypes: Map[ClassManifest[_], TypeHandler] = _
   private[this] var memoAssociations: Map[ClassManifest[_], Option[TypeHandler]] = _
-  
+
   def reload() {
     memoAssociations = Map.empty
     registeredTypes = Map.empty
-    for {provider <- ServiceLoader.load(classOf[TypeHandlerProvider])
-         typeHandler <- provider.handlers
-         handledType <- typeHandler.handledTypes} {
-           registeredTypes = registeredTypes.updated(handledType, typeHandler)
+    for {
+      provider <- ServiceLoader.load(classOf[TypeHandlerProvider]) ++ List(new BuiltinTypeHandlers)
+      typeHandler <- provider.handlers
+      handledType <- typeHandler.handledTypes
+    } {
+      registeredTypes = registeredTypes.updated(handledType, typeHandler)
     }
-     
   }
-  
+
   def handlerFor(type0: ClassManifest[_]): Option[TypeHandler] = {
     memoAssociations.get(type0) getOrElse {
-      val res = registeredTypes find (_._1 == type0) orElse 
-      (registeredTypes find (p => isSubtype(type0, p._1))) map (_._2)
-      
+      val res = registeredTypes find (_._1 == type0) orElse
+        (registeredTypes find (p => isSubtype(type0, p._1))) map (_._2)
+
       memoAssociations = memoAssociations.updated(type0, res)
       res
     }
   }
-  
+
   private[this] def isSubtype(a: ClassManifest[_], b: ClassManifest[_]): Boolean = {
     a <:< b || (b.erasure.isAssignableFrom(a.erasure) && (a.typeArguments.length == b.typeArguments.length
-        && a.typeArguments.zip(b.typeArguments).forall {
-      case (type1: ClassManifest[_], type2: ClassManifest[_]) => isSubtype(type1, type2)
-      case (type1: ClassManifest[_], NoManifest) => true // _ matches any class
-      case other => false 
-    })) || (a.erasure.isArray() && b.erasure.isArray() && 
+      && a.typeArguments.zip(b.typeArguments).forall {
+        case (type1: ClassManifest[_], type2: ClassManifest[_]) => isSubtype(type1, type2)
+        case (type1: ClassManifest[_], NoManifest) => true // _ matches any class
+        case other => false
+      })) || (a.erasure.isArray() && b.erasure.isArray() &&
         (b.erasure.getComponentType == classOf[AnyRef] || b.erasure.getComponentType.isAssignableFrom(a.erasure.getComponentType)))
   }
 }
